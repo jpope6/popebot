5381a5358152bf3dc2f0d50edcd53ef5ed21e173
jpope6 <imthepope@csu.fullerton.edu>
2024-04-21 22:02:02 -0700

    Added an evaluation function

--- engine/search.go
+++ engine/search.go
@@ -5,3 +5,60 @@ import "fmt"
 func searchPosition(depth int) {
 	fmt.Printf("bestmove d2d4\n")
 }
+
+func Evaluate(bs *BoardState) int {
+	score := 0
+
+	for piece := P; piece <= k; piece++ {
+		pieceColor := piece / 6
+		pieceType := piece % 6
+		bb := bs.Position.Pieces[pieceColor][pieceType]
+
+		for bb != 0 {
+			square := bb.GetLsbIndex()
+
+			score += mgMaterialScore[piece]
+			score += getMgPsqtScore(piece, square)
+
+			bb.PopBit(square)
+		}
+	}
+
+	multiplier := 1
+	if bs.Turn == Black {
+		multiplier = -1
+	}
+
+	return score * multiplier
+}
+
+func getMgPsqtScore(piece, square uint8) int {
+	switch piece {
+	case P:
+		return mgPawnTable[mirrorScore[square]]
+	case N:
+		return mgKnightTable[mirrorScore[square]]
+	case B:
+		return mgBishopTable[mirrorScore[square]]
+	case R:
+		return mgRookTable[mirrorScore[square]]
+	case Q:
+		return mgQueenTable[mirrorScore[square]]
+	case K:
+		return mgKingTable[mirrorScore[square]]
+	case p:
+		return -mgPawnTable[square]
+	case n:
+		return -mgKnightTable[square]
+	case b:
+		return -mgBishopTable[square]
+	case r:
+		return -mgRookTable[square]
+	case q:
+		return -mgQueenTable[square]
+	case k:
+		return -mgKingTable[square]
+	}
+
+	return 0
+}
--- popebot/main.go
+++ popebot/main.go
@@ -1,13 +1,15 @@
 package main
 
 import (
+	"fmt"
 	"popebot/engine"
 )
 
 const (
 	startFen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
-	testFen  = "4k3/8/8/8/8/8/8/R3K2R w KQ - 0 1"
+	testFen  = "8/8/8/8/8/8/3p4/8 w - - 0 1"
 	command  = "position startpos moves e2e4 e7e5 g1f3 b8c6"
+	debug    = true
 )
 
 func init() {
@@ -17,5 +19,12 @@ func init() {
 func main() {
 	var bs engine.BoardState
 
-	engine.UciLoop(&bs)
+	if debug {
+		bs.InitBoardState(testFen)
+		engine.PrintBoard(&bs)
+		fmt.Printf("\nScore: %d\n", engine.Evaluate(&bs))
+	} else {
+		engine.UciLoop(&bs)
+	}
+
 }
